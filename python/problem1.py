78fdbccb221f279fd19bc43e044f74b1914ca300
Dan Douthit <danieldouthit@gmail.com>
2024-12-23 23:39:19 -0700

    3 stars

--- python/problem1.py
+++ python/problem1.py
@@ -1,3 +1,17 @@
-#placeholder
-with open("../input/day1.txt") as inp:
-    print( list(zip(*(line.strip().split('\t') for line in inp))) )
+list1 = []
+list2 = []
+
+with open("/home/dandouthit/advent-of-code-2024/input/day1.txt", 'r') as file:
+    for line in file:
+        items = line.split()
+        list1.append(items[0])
+        list2.append(items[1])
+
+list1.sort()
+list2.sort()
+distance = 0
+
+for i in range(len(list1)):
+    distance += abs(int(list1[i]) - int(list2[i]))
+
+print(distance)
---
+++ python/problem1b.py
@@ -0,0 +1,15 @@
+list1 = []
+list2 = []
+score = 0
+
+with open("/home/dandouthit/advent-of-code-2024/input/day1.txt", 'r') as file:
+    for line in file:
+        items = line.split()
+        list1.append(int(items[0]))
+        list2.append(int(items[1]))
+
+
+for item in list1:
+    score = score + (item * list2.count(item))
+
+print(score)
