5730281a4585b95fa31dff7e6ee9c94c63b822dc
Dan Douthit <ddouthit@rubiconproject.com>
2024-12-29 16:39:16 -0700

    small tweaks

--- python/problem1b.py
+++ python/problem1b.py
@@ -2,12 +2,11 @@ list1 = []
 list2 = []
 score = 0
 
-with open("/../input/day1.txt", 'r') as file:
+with open("../input/day1.txt", 'r') as file:
     for line in file:
         items = line.split()
         list1.append(int(items[0]))
         list2.append(int(items[1]))
-
 
 for item in list1:
     score = score + (item * list2.count(item))
